dae0f1c7d707e74c3c306fa8f7af661584d00c52
Mitchell-Johnson <mitchj9@uw.edu>
2020-02-07 18:50:26 -0800

    temp

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./middleware/members');
 const logger = require('./middleware/logger.js');
 
 //Init express
@@ -8,31 +7,18 @@ const app = express();
 
 // init middleware
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({exnteded: false }));
+
 // logs the url and date/time of each get request
 // app.use(logger);
-
-// gets all members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
-
-// gets single member
-app.get('/api/members/:id', (req, res) => {
-    // returns true if the member is found in the json data - false if not
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-
-    // if member is found, send the json - if not, send bad request status and msg
-    if(found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id) ));
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
-    }
-});
 
 //set static folder
 app.use( express.static( path.join(__dirname, 'public') ) );
 
+// Members API routes
+app.use('/api/members', require('./routes/api/members.js'));
 
 const PORT = process.env.port || 5000;
 // tells the server to listen on port either the developer port or OTHER
